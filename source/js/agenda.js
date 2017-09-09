41cd1d0584d534bb098660dc48c018184282e3c1
Ivan Wei <scorpionseed@gmail.com>
2017-09-09 18:51:01 +0800

    update agenda info

---
+++ source/js/agenda.js
@@ -0,0 +1,26 @@
+(function($){
+  window.agendaDetail = function(date, time, room){
+    var currentAgendaContent = agendaData[date][time]['list'][room];
+    var swalOptions = {
+      imageWidth: 100,
+      confirmButtonText: '關閉'
+    }
+
+    if (currentAgendaContent.speaker) {
+      var currentSpeakerData = speakerData[currentAgendaContent.speaker];
+      var currntAgendaData = currentSpeakerData.agenda;
+
+      swalOptions.title = currentSpeakerData.name;
+      swalOptions.imageUrl = './images/speakers/' + currentSpeakerData.image;
+      swalOptions.html = '<h4>'+currntAgendaData.title+'</h4>';
+      if (currntAgendaData.outline)
+        swalOptions.html += '<div class="agenda-outline">' + currntAgendaData.outline + '<div>';
+    } else {
+      swalOptions.title = '我是犀牛啦！';
+      swalOptions.imageUrl = './images/jsdc-mascot-150.jpg';
+      swalOptions.html = '<h4>晚點告訴大家這位講者是誰</h4>';
+    }
+
+    swal(swalOptions).catch(swal.noop);
+  };
+})(jQuery);
