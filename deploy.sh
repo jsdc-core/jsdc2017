85b18180dffb00fb98c551c582c1dbe4926147ea
Ivan Wei <scorpionseed@gmail.com>
2017-09-20 22:17:28 +0800

    update deploy on travis

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # set -e # stop on error
 
-echo copy dist folder to deploy
-cp -R dist .deploy
 echo switch folder to deploy
 cd .deploy
+echo Remove all for .deploy
+rm -rf *
+echo copy dist folder to deploy
+cp -R ../dist/ .
+echo add new gh-pages files
+git add .
 echo commit changes
 git commit -m "deploy to gh-pages"
 echo push to remote gh-pages
