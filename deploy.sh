e872598d9a885e74965a72b5d08dd080e6056286
Ivan Wei <scorpionseed@gmail.com>
2017-09-20 17:58:22 +0800

    update deploy on travis

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # set -e # stop on error
 
-echo add dist folder
-git add -f dist
+echo copy dist folder to deploy
+cp -R dist .deploy
+echo switch folder to deploy
+cd .deploy
 echo commit changes
 git commit -m "deploy to gh-pages"
 echo push to remote gh-pages
-git push origin `git subtree split --prefix dist`:gh-pages --force
+git push -f
 echo All done!
