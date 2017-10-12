e4302334147371daa60aec17e195f989c9dc9c20
Ivan Wei <scorpionseed@gmail.com>
2017-10-13 02:27:39 +0800

    update index.html

--- source/js/main.js
+++ source/js/main.js
@@ -56,27 +56,55 @@
 
     // });
 
-    // if (location.pathname === '/')
-      // setInterval(function(){
-      //   var specifyDay = new Date('2017/10/01 23:59:59').getTime();
-      //   var now = new Date().getTime();
-      //   var diff = new Date(specifyDay - now).getTime();
-      //   var util = {
-      //     seconds: 1000,
-      //     minutes: 1000*60,
-      //     hours: 1000*60*60,
-      //     date: 1000*60*60*24,
-      //   }
+    if (location.pathname === '/')
+      var countdown = setInterval(function(){
+        var specifyDay = null;
+        var nightTicketEndDay = new Date('2017/10/18 22:00:00').getTime();
+        var confStartDay = new Date('2017/11/04 09:00:00').getTime();
+        var confEndtDay = new Date('2017/11/05 17:00:00').getTime();
+        var countdownTitle = document.getElementsByClassName('content')[0];
+        var ticketButtonText = document.getElementsByClassName('ticket-booking')[0];
 
-      //   var date = Math.floor(diff / util.date);
-      //   var hours = Math.floor((diff - (date * util.date)) / util.hours);
-      //   var minutes = Math.floor((diff - ((date * util.date) + (hours * util.hours))) / util.minutes);
-      //   var seconds = Math.floor((diff - ((date * util.date) + (hours * util.hours) + (minutes * util.minutes))) / util.seconds);
+        if (nightTicketEndDay > Date.now()) {
+          specifyDay = nightTicketEndDay;
+          countdownTitle.innerHTML = 'countdown ticket';
 
-      //   $('.date').text(date.toString().length === 1?'0'+ date:date);
-      //   $('.hours').text(hours.toString().length === 1?'0'+ hours:hours);
-      //   $('.minutes').text(minutes.toString().length === 1?'0'+ minutes:minutes);
-      //   $('.seconds').text(seconds.toString().length === 1?'0'+ seconds:seconds);
-      // }, 1000);
+        } else if (confStartDay > Date.now()) {
+          specifyDay = confStartDay;
+          countdownTitle.innerHTML = 'jsdc 2017 countdown';
+
+        } else if (confEndtDay > Date.now()) {
+          specifyDay = confEndtDay;
+          countdownTitle.innerHTML = 'countdown to the end of jsdc';
+
+        } else {
+          countdownTitle.innerHTML = 'see you jsdc 2018';
+          clearInterval(countdown);
+          return;
+        }
+
+        if (specifyDay !== nightTicketEndDay) {
+          ticketButtonText.text = 'SOLD OUT';
+        }
+
+        var now = Date.now();
+        var diff = new Date(specifyDay - now).getTime();
+        var util = {
+          seconds: 1000,
+          minutes: 1000*60,
+          hours: 1000*60*60,
+          date: 1000*60*60*24,
+        }
+
+        var date = Math.floor(diff / util.date);
+        var hours = Math.floor((diff - (date * util.date)) / util.hours);
+        var minutes = Math.floor((diff - ((date * util.date) + (hours * util.hours))) / util.minutes);
+        var seconds = Math.floor((diff - ((date * util.date) + (hours * util.hours) + (minutes * util.minutes))) / util.seconds);
+
+        $('.date').text(date.toString().length === 1?'0'+ date:date);
+        $('.hours').text(hours.toString().length === 1?'0'+ hours:hours);
+        $('.minutes').text(minutes.toString().length === 1?'0'+ minutes:minutes);
+        $('.seconds').text(seconds.toString().length === 1?'0'+ seconds:seconds);
+      }, 1000);
 
 })(jQuery);
